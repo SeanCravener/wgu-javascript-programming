91e0433435089978dd0e24812581492dcfeac22e
Sean Cravener <scravenerpat@gmail.com>
2024-11-05 12:46:16 -0500

    Set up routing to meet requirement D. Redirects from default URL to /map.

--- world-map-project/src/app/app.routes.ts
+++ world-map-project/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ContainerComponent } from './container/container.component';
+export const routes: Routes = [
+  { path: 'map', component: ContainerComponent },
+  { path: '', redirectTo: '/map', pathMatch: 'full' },
+  { path: '**', redirectTo: '/map' },
+];
