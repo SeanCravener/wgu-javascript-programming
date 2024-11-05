a9f775ca85e27d099d410db4d540b01a49435d59
Sean Cravener <scravenerpat@gmail.com>
2024-11-05 12:50:24 -0500

    Got basic world map SVG set up in component for requirement B.

--- world-map-project/src/app/world-map-api.service.ts
+++ world-map-project/src/app/world-map-api.service.ts
@@ -1,31 +1,3 @@
-// import { Injectable } from '@angular/core';
-
-// const API_URL = 'https://api.worldbank.org/v2/country/';
-
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class WorldMapApiService {
-//   constructor() {}
-
-//   getCountryDetailsByCode(countryCode: string) {
-//     return fetch(`${API_URL}${countryCode}?format=json`)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         const countryData = data[1][0];
-//         return {
-//           name: countryData.name,
-//           capitalCity: countryData.capitalCity,
-//           region: countryData.region.value,
-//           incomeLevel: countryData.incomeLevel.value,
-//           latitude: countryData.latitude,
-//           longitude: countryData.longitude,
-//         };
-//       });
-//   }
-// }
-// Rewrite all the code above to use the HttpClient service instead of the fetch API. The service should still return the same data structure.
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
